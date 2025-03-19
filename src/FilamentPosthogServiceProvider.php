8f6529648a0d6feacdaa078548f7dbd4b006302d
danielhe4rt <6912596+danielhe4rt@users.noreply.github.com>
2025-03-19 19:40:47 +0000

    Fix styling

--- src/FilamentPosthogServiceProvider.php
+++ src/FilamentPosthogServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace DanielHe4rt\FilamentPosthog;
 
+use DanielHe4rt\FilamentPosthog\Commands\FilamentPosthogCommand;
+use DanielHe4rt\FilamentPosthog\Testing\TestsFilamentPosthog;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use DanielHe4rt\FilamentPosthog\Commands\FilamentPosthogCommand;
-use DanielHe4rt\FilamentPosthog\Testing\TestsFilamentPosthog;
 
 class FilamentPosthogServiceProvider extends PackageServiceProvider
 {
